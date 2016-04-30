ece52163b68ac2b57f5cc97e5112345dc9ed0bca
mgoo <andrewmcghienz@gmail.com>
2016-04-30 13:01:39 +1200

    Added Sugar ORM again

---
+++ app/src/main/java/io/hackathon/hackathon2016/Agency.java
@@ -0,0 +1,11 @@
+package io.hackathon.hackathon2016;
+
+import com.orm.SugarRecord;
+import com.orm.dsl.Table;
+
+/**
+ * Created by mgoo on 30/04/16.
+ */
+@Table
+public class Agency extends SugarRecord{
+}
--- app/src/main/java/io/hackathon/hackathon2016/MainActivity.java
+++ app/src/main/java/io/hackathon/hackathon2016/MainActivity.java
@@ -10,4 +10,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void updateDatabase() {
+        if (!this.needsToUpdate())return;
+    }
+
+    public boolean needsToUpdate(){
+        return false;
+    }
 }
