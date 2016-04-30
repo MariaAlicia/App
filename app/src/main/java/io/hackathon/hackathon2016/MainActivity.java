0c68a8583988b3aabd844627600777361e36c3d4
MariaAlicia <maria.ahkit@hotmail.com>
2016-04-30 12:12:42 +1200

    Go button added
    Go button works - currently only does pop up text but will do a new
    content view

--- app/src/main/java/io/hackathon/hackathon2016/MainActivity.java
+++ app/src/main/java/io/hackathon/hackathon2016/MainActivity.java
@@ -2,12 +2,22 @@ package io.hackathon.hackathon2016;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button b1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+
+        b1=(Button)findViewById(R.id.go);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"YOUR MESSAGE",Toast.LENGTH_LONG).show();
+            }
+        });
+}}
