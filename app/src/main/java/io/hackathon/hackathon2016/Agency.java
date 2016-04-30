806223861574eb41dacf9de29db7298f71954aa1
mgoo <andrewmcghienz@gmail.com>
2016-04-30 13:29:22 +1200

    added columns and stuff

--- app/src/main/java/io/hackathon/hackathon2016/Agency.java
+++ app/src/main/java/io/hackathon/hackathon2016/Agency.java
@@ -8,4 +8,24 @@ import com.orm.dsl.Table;
  */
 @Table
 public class Agency extends SugarRecord{
+    String agency_id,agency_name,agency_url,agency_timezone,agency_lang,agency_phone,agency_fare_url;
+    private long id;
+
+    public Agency(){
+
+    }
+
+    public Agency(String[] values){
+        this.agency_id = values[0];
+        this.agency_name = values[1];
+        this.agency_url = values[2];
+        this.agency_timezone = values[3];
+        this.agency_lang = values[4];
+        this.agency_phone = values[5];
+        this.agency_fare_url = values[6];
+    }
+
+    public Long getId(){
+        return this.id;
+    }
 }
